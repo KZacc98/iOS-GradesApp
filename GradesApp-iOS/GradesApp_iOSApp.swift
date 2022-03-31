627e88f8ddfc91a5a58b265b6ae4dccaa4723c19
kamilzachara <kamil.zachara98@gmail.com>
2022-04-01 01:59:08 +0200

    Initial Commit

---
+++ GradesApp-iOS/GradesApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradesApp_iOSApp.swift
+//  GradesApp-iOS
+//
+//  Created by Kamil Zachara on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GradesApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
